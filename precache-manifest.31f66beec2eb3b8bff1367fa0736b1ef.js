03ec76d827027c21ad1f49ba8d83fd498ac7f05c
park <yopy0817@naver.com>
2019-12-23 01:34:36 +0900

    Updates

---
+++ precache-manifest.31f66beec2eb3b8bff1367fa0736b1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5d862a3e55192160fbcbbc7c4c20ce",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "961a7bb924edb21c7475",
+    "url": "/react_app/static/css/main.5743d349.chunk.css"
+  },
+  {
+    "revision": "249d483929821a58bcfb",
+    "url": "/react_app/static/js/2.28db6f8c.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react_app/static/js/2.28db6f8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "961a7bb924edb21c7475",
+    "url": "/react_app/static/js/main.00629770.chunk.js"
+  },
+  {
+    "revision": "662a6f726fed232e2105",
+    "url": "/react_app/static/js/runtime-main.c81f5c3e.js"
+  }
+]);
